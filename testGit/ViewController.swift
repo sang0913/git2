9f20efb0420802603b88f758581c5002a925ea69
sang0913 <74355128+sang0913@users.noreply.github.com>
2021-06-08 18:22:44 +0700

    new push branch1

--- testGit/ViewController.swift
+++ testGit/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("commit2 to branch master")
+        print("commit3 to branch master")
     }
 
 
