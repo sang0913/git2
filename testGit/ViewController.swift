5a5b1c82e81eaf1b423731e16a2e1a6edf530f19
sang0913 <74355128+sang0913@users.noreply.github.com>
2021-06-08 18:11:04 +0700

    add commit2 to branch master

--- testGit/ViewController.swift
+++ testGit/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("commit2 to branch master")
     }
 
 
